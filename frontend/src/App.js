6fb6395888ebe185c1608add5c663554dc018865
DHUNTLEYPRO <62427154+dhuntleypro@users.noreply.github.com>
2022-09-10 10:25:57 -0400

    auto sign out user once token  expires

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,63 @@
-import {
-  BrowserRouter,
-  Navigate,
-  NavLink,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import PremiumContent from './pages/PremiumContent';
 import Register from './pages/Register';
-import PrivateRoute from './routes/PrivateRoute';
-import { getToken } from './service/AuthService';
+// import PrivateRoute from './routes/PrivateRoute';
+import {
+  getToken,
+  getUser,
+  resetUserSession,
+  setUserSession,
+} from './service/AuthService';
+
+const verifyTokenAPIURL =
+  'https://srut07d9ka.execute-api.us-east-1.amazonaws.com/prod/verify';
 
 function App() {
+  const [isAuthenicating, setAuthenicating] = useState(true);
+  useEffect(() => {
+    const token = getToken();
+    if (
+      token === 'undefined' ||
+      token === undefined ||
+      token === null ||
+      !token
+    ) {
+      return;
+    }
+
+    const requestConfig = {
+      headers: {
+        // you can find in aws, under api gateway | api keys | show
+        'x-api-key': 'ygzOzmOjbpaOJNWiwVR582KMw0WdGe1E9HnV8dEZ',
+      },
+    };
+
+    const requestBody = {
+      user: getUser(),
+      token: token,
+    };
+
+    axios
+      .post(verifyTokenAPIURL, requestBody, requestConfig)
+      .then((response) => {
+        setUserSession(response.data.user, response.data.token);
+        setAuthenicating(false);
+      })
+      .catch(() => {
+        resetUserSession();
+        setAuthenicating(false);
+      });
+  }, []);
+
+  const token = getToken();
+  if (isAuthenicating && token) {
+    return <div className="content">Authenicating...</div>;
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -32,6 +77,14 @@ function App() {
 
             {/* Private Routes (not coded optimally - refresh fuction on login)*/}
             <Route exact path="/premium-content" element={<PremiumContent />} />
+
+            {/* <PrivateRoute>
+              <Route
+                exact
+                path="/premium-content"
+                element={<PremiumContent />}
+              />
+            </PrivateRoute> */}
           </Routes>
         </div>
       </BrowserRouter>
