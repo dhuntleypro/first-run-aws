d8d977a10e505da4467e1bab0a35bfc9d8127244
DHUNTLEYPRO <62427154+dhuntleypro@users.noreply.github.com>
2022-09-10 08:43:37 -0400

    premium page working (shows current name of user)

--- frontend/src/pages/PremiumContent.js
+++ frontend/src/pages/PremiumContent.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUser, resetUserSession } from '../service/AuthService';
 
 const PremiumContent = () => {
-  return <div>this is the Premium Content page</div>;
+  const navigate = useNavigate();
+
+  const user = getUser();
+  const name = user !== 'undefined' && user ? user.name : '';
+
+  const logoutHandler = () => {
+    resetUserSession();
+    navigate('/login');
+  };
+  return (
+    <div>
+      Hello {name}! You have been logged in !!! Welcome to the premium content.
+      <input type="button" value="Logout" onClick={logoutHandler}></input>
+    </div>
+  );
 };
 
 export default PremiumContent;
